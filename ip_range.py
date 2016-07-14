abc2e48976000c72e9eaf8a38401a2d7f686fb4f
zwb <zwb35.gmail.com>
2016-07-14 18:43:36 +0800

    get ip

---
+++ ip_range.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python
+# -*- coding:utf-8 -*-
+
+
+def ip2num(ip):
+    ip = [int(x) for x in ip.split('.')]
+    # print ip
+    return ip[0] << 24 | ip[1] << 16 | ip[2] << 8 | ip[3]
+
+
+def num2ip(num):
+    return '%s.%s.%s.%s' % ((num & 0xff000000) >> 24,
+                            (num & 0x00ff0000) >> 16,
+                            (num & 0x0000ff00) >> 8,
+                            num & 0x000000ff)
+
+
+def get_ip(ip):
+    start, end = [ip2num(x) for x in ip.split('-')]
+    return [num2ip(num) for num in range(start, end+1) if num & 0xff]
+
+if __name__ == '__main__':
+    iplist = get_ip('172.16.3.1-172.16.3.21')
+    a = [i for i in iplist]
+    print a
